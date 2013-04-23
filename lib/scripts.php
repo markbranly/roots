2a3dc6b584900eb90a900a0f98b8f395dcc990b4
Mark Branly <mark@registeredcreative.com>
2013-04-23 13:13:52 -0400

    Changes to css/less structuring

--- lib/scripts.php
+++ lib/scripts.php
@@ -3,8 +3,8 @@
  * Enqueue scripts and stylesheets
  *
  * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/bootstrap.css
- * 2. /theme/assets/css/bootstrap-responsive.css
+ * --1. /theme/assets/css/bootstrap.css
+ * --2. /theme/assets/css/bootstrap-responsive.css
  * 3. /theme/assets/css/app.css
  * 4. /child-theme/style.css (if a child theme is activated)
  *
@@ -15,8 +15,9 @@
  * 4. /theme/assets/js/main.js    (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
-  wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
+  // MPB: Don't queue up bootstrap or bootstrap-responsive, they are being imported into app.less
+  // wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
+  // wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // Load style.css from child theme
